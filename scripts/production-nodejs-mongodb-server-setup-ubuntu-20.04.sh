1bd8750a3b17bbdf92e1b84dd98ba7ac189c3a76
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-04-05 19:12:41 +0200

    Update production-nodejs-mongodb-server-setup-ubuntu-20.04.sh

--- scripts/production-nodejs-mongodb-server-setup-ubuntu-20.04.sh
+++ scripts/production-nodejs-mongodb-server-setup-ubuntu-20.04.sh
@@ -1,90 +1,69 @@
 #!/usr/bin/env bash
 
+# install build essentials
+sudo apt install -y build-essential wget gnupg
+
 echo "
 ----------------------
   NODE & NPM
 ----------------------
 "
-
 # add nodejs LTS ppa (personal package archive) from nodesource
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-
 # install nodejs and npm
-sudo apt-get install -y nodejs
-
-# install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install --no-install-recommends -y yarn
-
-# install build essentials
-sudo apt-get install -y build-essential wget gnupg
-
+sudo apt install -y nodejs
 
 echo "
 ----------------------
   MONGODB
 ----------------------
 "
-
 # import mongodb 7.0 public gpg key
 curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
 apt-key list
-
 # create the /etc/apt/sources.list.d/mongodb-org-7.0.list file for mongodb
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-
 # reload local package database
-sudo apt-get update
-
+sudo apt update
 # install the latest version of mongodb
-sudo apt-get install -y mongodb-org
-
+sudo apt install -y mongodb-org
 # start mongodb
 sudo systemctl start mongod
-
 # set mongodb to start automatically on system startup
 sudo systemctl enable mongod
 
-# status mongodb
-# sudo systemctl status mongod
-
 echo "
-----------------------
-  PM2
-----------------------
+-----------------------------------
+    installing pisignage-server
+-----------------------------------
 "
+sudo git clone https://github.com/colloqi/pisignage-server
+cd pisignage-server
+npm install
 
-# install pm2 with npm
-sudo npm install -g pm2
-
-# set pm2 to start automatically on system startup
-sudo pm2 startup systemd
-
+#create media and thumbnail directory
+cd ..
+mkdir media
+sudo chmod 755 -R ./media
 
 echo "
 ----------------------
   NGINX
 ----------------------
 "
-
 # install nginx
-sudo apt-get install -y nginx
-
+sudo apt install -y nginx
 
 echo "
 ----------------------
   UFW (FIREWALL)
 ----------------------
 "
-
 # allow ssh connections through firewall
 sudo ufw allow OpenSSH
-
 # allow http & https through firewall
 sudo ufw allow 'Nginx Full'
-
 # enable firewall
 sudo ufw --force enable
